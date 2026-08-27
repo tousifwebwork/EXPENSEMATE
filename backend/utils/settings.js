be5bc86b0d91144dbcf93af2d3a4b93ca8873881
Mohammad Tousif Sunkat <tousifwebwork@gmail.com>
2026-08-27 15:46:35 +0530

    Created and tested Group's API

--- backend/utils/settings.js
+++ backend/utils/settings.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", require("../router/auth/authRoutes"));
 app.use("/api/user",require("../router/user/userRoutes"));
 app.use("/api/friends",require("../router/friend/friendRoutes"));
+app.use("/api/groups", require("../router/group/groupRoutes"));
 }
 
 module.exports = setup;
