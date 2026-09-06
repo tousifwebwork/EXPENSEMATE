85d47ca4bd0413681d44f8865c067c8d3a7b572b
Mohammad Tousif Sunkat <tousifwebwork@gmail.com>
2026-09-06 16:56:59 +0530

    fix vite allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["expensemate-1-fmj3.onrender.com"],
+  },
 })
