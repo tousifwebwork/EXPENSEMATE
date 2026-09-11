836a91f2e9944532cca6072b092060e16d6ebcb6
Mohammad Tousif Sunkat <tousifwebwork@gmail.com>
2026-09-11 10:54:53 +0530

    Minor changes to handle login state in frontend

--- frontend/src/pages/protected/Protected.jsx
+++ frontend/src/pages/protected/Protected.jsx
@@ -22,7 +22,7 @@ const Protected = () => {
       try { 
         const payload = JSON.parse(atob(token.split(".")[1]));
         const currentTime = Date.now() / 1000;
-        if (payload.exp && payload.exp < currentTime) {
+        if (payload.exp && payload.exp <= currentTime) {
           localStorage.removeItem("token");
           setIsAuthenticated(false);
           setLoading(false);
