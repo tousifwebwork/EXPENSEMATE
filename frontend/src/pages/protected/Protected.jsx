ffb39f815da78a9b1b3a0f8b608e662980b289db
Mohammad Tousif Sunkat <tousifwebwork@gmail.com>
2026-09-11 10:53:44 +0530

    Minor changes to handle login state in frontend

--- frontend/src/pages/protected/Protected.jsx
+++ frontend/src/pages/protected/Protected.jsx
@@ -1,10 +1,50 @@
-import { Navigate, Outlet } from "react-router-dom";
+
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Protected = () => {
-  const token = localStorage.getItem("token");
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  if (!token) {
-    return <Navigate to="/login" replace />;
+  const location = useLocation();
+
+
+  useEffect(() => {   
+    const checkAuth = () => {
+      const token = localStorage.getItem("token");
+
+       if (!token) {
+        setIsAuthenticated(false);
+        setLoading(false);
+        return;
+      }
+
+      try { 
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        const currentTime = Date.now() / 1000;
+        if (payload.exp && payload.exp < currentTime) {
+          localStorage.removeItem("token");
+          setIsAuthenticated(false);
+          setLoading(false);
+          return;
+        }
+        setIsAuthenticated(true);
+        setLoading(false);
+      } catch (error) {
+        localStorage.removeItem("token");
+        setIsAuthenticated(false);
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+   if (loading) {
+    return <div>Loading...</div>;
+  }
+
+   if (!isAuthenticated) {
+    return (<Navigate to="/login"state={{ from: location }} replace />);
   }
 
   return <Outlet />;
